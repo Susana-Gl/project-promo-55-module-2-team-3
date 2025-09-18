db7995f7c6312285eddd268d6287dd2b0249ac86
irati88 <irati.a88@gmail.com>
2025-09-18 20:48:14 +0200

    create share.js for dom-to-image

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import "./share.js";
+
 console.log(">> Ready :)");
 
 const background = document.getElementById("background");
---
+++ src/js/share.js
@@ -0,0 +1,16 @@
+// import domToImage from "dom-to-image"; 
+
+// const downloadButton = document.querySelector(".download-btn");
+
+// downloadButton.addEventListener("click", (event) => {
+//   event.preventDefault();
+
+//   domToImage.toJpeg(document.getElementById("card-preview"), { quality: 0.95})
+//   .then((dataUrl) => {
+//     const link = document.createElement("a");
+//     link.download = "my-card.jpeg";
+//     link.href = dataUrl;
+//     link.click();
+//   });
+// });
+
